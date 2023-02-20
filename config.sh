ad0e33188807edccf0998adae0364ca5ac8e1e74
comar <saywooong@gmail.com>
2023-02-20 20:43:29 +0900

    update: general system

--- config.sh
+++ config.sh
@@ -1,24 +1,25 @@
 cat /proc/1/cgroup | grep docker
 
+export width=400
+export height=400
+export optimizer=adam
+
 if [[ $(hostname | grep mark11) ]]; then
     # IN DOCKER CONTAINER MARK11
+    export patch_size=48
     export n_gpu=4
-    export width=400
-    export height=400
     export CUDA_VISIBLE_DEVICES=4,5,6,7
-    export optimizer=adam
+    export condapath=/opt/conda/etc/profile.d/conda.sh
 elif [[ $(hostname | grep mark12) ]]; then
     # IN DOCKER CONTAINER MARK12
+    export patch_size=64
     export n_gpu=1
-    export width=400
-    export height=400
     export CUDA_VISIBLE_DEVICES=4
-    export optimizer=adam
+    export condapath=/opt/conda/etc/profile.d/conda.sh
 else
     # IN LOCAL SYSTEM
+    export patch_size=48
     export n_gpu=1
-    export width=400
-    export height=400
     export CUDA_VISIBLE_DEVICES=0
-    export optimizer=sgd
+    export condapath=$HOME/anaconda3/etc/profile.d/conda.sh
 fi
--- train_step2.sh
+++ train_step2.sh
@@ -5,5 +5,4 @@ source ~/anaconda3/etc/profile.d/conda.sh
 conda activate SinNeRF
 
 # STAGE 2
-python train.py  --dataset_name blender_ray_patch_1image_rot3d  --root_dir  DATASET/synthetic_SinNeRF/lego   --N_importance 64 --img_wh $width $height --num_epochs 2000 --batch_size 1  --optimizer $optimizer --lr 5e-5  --lr_scheduler steplr --decay_step 500 1000 --decay_gamma 0.5  --exp_name lego_s6_4ft --with_ref --patch_size 64 --sW 4 --sH 4 --proj_weight 1 --depth_smooth_weight 0  --dis_weight 0.01 --num_gpus $n_gpu --load_depth --depth_type nerf --model sinnerf --depth_weight 8 --vit_weight 0 --pt_model xxx.ckpt --nerf_only  --scan 4
-
+python train.py  --dataset_name blender_ray_patch_1image_rot3d  --root_dir  DATASET/synthetic_SinNeRF/lego   --N_importance 64 --img_wh $width $height --num_epochs 2000 --batch_size 1  --optimizer $optimizer --lr 5e-5  --lr_scheduler steplr --decay_step 500 1000 --decay_gamma 0.5  --exp_name lego_s6_4ft --with_ref --patch_size $patch_size --sW 4 --sH 4 --proj_weight 1 --depth_smooth_weight 0  --dis_weight 0.01 --num_gpus $n_gpu --load_depth --depth_type nerf --model sinnerf --depth_weight 8 --vit_weight 0 --pt_model xxx.ckpt --nerf_only  --scan 4
